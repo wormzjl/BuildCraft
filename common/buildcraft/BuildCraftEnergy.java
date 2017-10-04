d68f57328dbe819dad9f1b3fb88d0abd9a33aa70
wormzjl <wormzjl@gmail.com>
2017-10-05 00:07:31 +0200

    Disable oil refining

--- common/buildcraft/BuildCraftEnergy.java
+++ common/buildcraft/BuildCraftEnergy.java
@@ -288,9 +288,6 @@ public class BuildCraftEnergy extends BuildCraftMod {
 		}
 		MinecraftForge.EVENT_BUS.register(BucketHandler.INSTANCE);
 
-		BuildcraftRecipeRegistry.refinery.addRecipe("buildcraft:fuel", new FluidStack(fluidOil, 1), new FluidStack(
-				fluidFuel, 1), 120, 1);
-
 		BuildcraftFuelRegistry.fuel.addFuel(fluidOil, fuelOilEnergyOutput, (int) (5000 * fuelOilMultiplier));
 		BuildcraftFuelRegistry.fuel.addFuel(fluidFuel, fuelFuelEnergyOutput, (int) (25000 * fuelFuelMultiplier));
 
